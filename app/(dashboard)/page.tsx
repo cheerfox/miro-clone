404c26b2db56559172bf4679b85199d2f4b3166c
Wang ChunChi <wangchunchi@WangdeMacBook-Pro.local>
2024-07-16 17:02:58 +0800

    feature: Add organization empty state

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { useOrganization } from "@clerk/nextjs";
+import { EmptyOrg } from "./_components/empty-org";
+
 const DashboardPage = () => {
+const { organization } = useOrganization();
+
   return (
-    <div>
-      Dashboard root page
+    <div className="flex-1 h-[calc(100%-80px)]">
+      {
+        !organization ? (
+          <EmptyOrg />
+        ) : (
+          <p>Board List</p>
+        )
+      }
     </div>
   );
 };
